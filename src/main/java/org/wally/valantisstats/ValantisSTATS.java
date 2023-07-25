d53274a59f151b32053be128b60edbc7841aff76
Wally17 <wally.connor17@gmail.com>
2023-07-25 20:45:56 +0800

    Initialise the project

---
+++ src/main/java/org/wally/valantisstats/ValantisSTATS.java
@@ -0,0 +1,17 @@
+package org.wally.valantisstats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ValantisSTATS extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
