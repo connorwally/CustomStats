4f46426bebbd66aeb980ca5bdb402cc4e5efe689
Wally17 <wally.connor17@gmail.com>
2023-07-25 22:47:54 +0800

    Added the command executor

---
+++ src/main/java/org/wally/valantisstats/Commands/ModifyStats.java
@@ -0,0 +1,14 @@
+package org.wally.valantisstats.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class ModifyStats implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+
+        return false; }
+}
--- src/main/java/org/wally/valantisstats/ValantisSTATS.java
+++ src/main/java/org/wally/valantisstats/ValantisSTATS.java
@@ -1,12 +1,16 @@
 package org.wally.valantisstats;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.wally.valantisstats.Commands.ModifyStats;
+
+import java.util.Objects;
 
 public final class ValantisSTATS extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        Objects.requireNonNull(this.getCommand("valstats")).setExecutor(new ModifyStats());
 
     }
 
