364711a2bcae5235bc518645d5d2a5f01f6fd94c
Wally17 <wally.connor17@gmail.com>
2023-07-31 17:40:58 +0800

    Ensuring that the amount can be parsed to integer, halfway through adding the set command

--- src/main/java/org/wally/valantisstats/Commands/ModifyStats.java
+++ src/main/java/org/wally/valantisstats/Commands/ModifyStats.java
@@ -1,6 +1,5 @@
 package org.wally.valantisstats.Commands;
 
-import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,35 +10,52 @@ import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 import org.wally.valantisstats.ValantisSTATS;
 
-import javax.naming.Name;
-
 public class ModifyStats implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
 
+        // /valstats add <stat> <amount>
 
+        // Ensure a player is running the command
         if(!(sender instanceof Player)){
             ValantisSTATS.getPlugin().getLogger().info("The console cannot run this command");
             return false;
         }
 
+        // Ensure enough arguments are provided
         if(args.length < 3){
             ValantisSTATS.getPlugin().getLogger().info("Not enough arguments provided");
             return false;
         }
 
+
         Player player = (Player) sender;
 
-        if(!args[0].equals("add")){
+        // Ensure that a valid command has been sent
+        if(!(args[0].equals("add") || args[0].equals("set"))){
             ValantisSTATS.getPlugin().getLogger().info("You need to use \"add\"");
             return false;
         }
 
         PersistentDataContainer c = player.getPersistentDataContainer();
 
+        // Ensure that the entered amount can be converted to an integer.
+        int amount;
+        try{
+            amount = Integer.parseInt(args[2]);
+        }
+        catch(NumberFormatException e){
+            ValantisSTATS.getPlugin().getLogger().info("The amount must be an integer");
+            return false;
+        }
+
         // Perform the add stats on the player
-        AddStats(args[1], Integer.parseInt(args[2]), c);
+        if(args[0].equals("add"))
+            AddStats(args[1], amount, c);
+
+        if(args[0].equals("set"))
+            SetStats(args[1], amount, c);
 
         return true;
     }
@@ -57,9 +73,11 @@ public class ModifyStats implements CommandExecutor {
         }
         // If the persistent data already exists, add onto it and reset it.
         else{
-            int currentamount = container.get(new NamespacedKey(ValantisSTATS.getPlugin(), key), PersistentDataType.INTEGER);
-            currentamount += added;
+            int currentamount = container.get(new NamespacedKey(ValantisSTATS.getPlugin(), key), PersistentDataType.INTEGER) + added;
             container.set(new NamespacedKey(ValantisSTATS.getPlugin(), key), PersistentDataType.INTEGER, currentamount);
         }
+    }
+
+    void SetStats(String key, int added, PersistentDataContainer container){
     }
 }
